c62da0ad6530fd36f64fecbc440cda1e2ff2d8ea
Wojtuch566 <dromawojciech@gmail.com>
2024-11-21 09:02:14 +0100

    Template of WeatherApp class

--- lab-d/pogodynka.js
+++ lab-d/pogodynka.js
@@ -1,1 +1,31 @@
+const WeatherApp = class {
+    constructor(apiKey, resultsBlockSelector) {
 
+    }
+
+    getCurrentWeather(query) {
+
+    }
+
+    getForecast(query) {
+
+    }
+
+    getWeather(query) {
+
+    }
+
+    drawWeather() {
+
+    }
+
+    createWeatherBlock(dateString, temperature, feelsLikeTemperature, iconName, description) {
+
+    }
+}
+
+document.weatherApp = new WeatherApp("7093d486057c9e3ae1d705d22654e9b9", "#weather-result");
+document.querySelector("#weatherCheck").addEventListener("click", function () {
+    const query = document.querySelector("#weatherLocation").value;
+    document.weatherApp.getCurrentWeather(query);
+});
